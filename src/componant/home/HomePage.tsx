6a32a463b6d3dfad85e238952216559948e63300
NA-Asraful-Khan <soyonshikhor@gmail.com>
2025-06-17 04:06:00 +0600

    Markup Statistics Section

--- src/componant/home/HomePage.tsx
+++ src/componant/home/HomePage.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./HeroSection";
+import StatisticsSection from "./StatisticsSection";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <StatisticsSection />
     </>
   );
 };
