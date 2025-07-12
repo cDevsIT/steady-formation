5de4e6c628fdbed92e54f71e573772528d75cf9a
rumon2u <rafiqul.islam@synesisit.info>
2025-07-12 12:22:16 +0600

    api url change

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
-    BASE_URL: 'http://localhost:8000/api',
+    BASE_URL: 'https://api.cdevs.com.bd/api',
+    // BASE_URL: 'http://localhost:8000/api',
     ENDPOINTS: {
         AUTH: {
             LOGIN: '/auth/login',
